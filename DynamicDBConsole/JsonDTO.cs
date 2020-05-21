58587110e6e29c299a75aa5bb3ecdef4ed85e252
konstantin.borisov-at-908478757030 <borisovke@gmail.com>
2020-05-21 22:52:29 +0600

    Create DynamoDB table and get a random verb

---
+++ DynamicDBConsole/JsonDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DynamicDBConsole
+{
+    public class JsonDTO
+    {
+        public string Infinitive { get; set; }
+        public string TenseKey { get; set; }
+        public string Word { get; set; }
+        public bool IsIrregular { get; set; }
+        public int StartIdx { get; set; }
+        public int EndIdx { get; set; }
+    }
+}
