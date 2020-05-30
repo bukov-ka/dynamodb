d8f6698fb564480a22c98167de54f708956e108f
konstantin.borisov-at-908478757030 <borisovke@gmail.com>
2020-05-30 13:19:00 +0600

    Columns list added

--- DynamoDBSQL/dynamosql/src/app/live-sql/live-sql.component.ts
+++ DynamoDBSQL/dynamosql/src/app/live-sql/live-sql.component.ts
@@ -11,6 +11,7 @@ export class LiveSqlComponent implements OnInit {
   sqlText:string = "SELECT * FROM Music";
   resultData:Array<any>;
   resultError:string;
+  columns:Array<string>;
   constructor() { 
   }
 
@@ -23,14 +24,24 @@ export class LiveSqlComponent implements OnInit {
     let self = this;
     alasql.default.promise(sqlWithCsvNames)    
     .then(function(data){
-      console.log('It works! I think...');
       self.resultData=data;
       self.resultError=null;
+      self.columns=self.getColumns(data);
       console.log(data);
     }).catch(function(err){
       console.log('Error:', err);
       self.resultError = err;
       self.resultData = null;
+      self.columns=null;
     });
+  }
+
+  getColumns(content:Array<any>):Array<string> {
+    let columns:Array<string>=[];
+    if(!content) return columns;
+    Object.keys(content[0]).forEach(key=>{
+      columns.push(key);
+    });
+    return columns;
   }
 }
