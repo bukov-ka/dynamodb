f1ae069b0d22e55e12b5718a26774168a91ce35b
konstantin.borisov-at-908478757030 <borisovke@gmail.com>
2020-07-10 01:02:02 +0600

    Material design to the GSI component

--- DynamoDBSQL/dynamosql/src/app/app.module.ts
+++ DynamoDBSQL/dynamosql/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';  
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LiveSqlComponent } from './live-sql/live-sql.component';
 import { TableViewComponent } from './table-view/table-view.component';
 import { GsiSolutionComponent } from './gsi-solution/gsi-solution.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +25,13 @@ import { GsiSolutionComponent } from './gsi-solution/gsi-solution.component';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule,    
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    MatInputModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatSelectModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
