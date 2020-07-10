a30b197c8313db77274bd8a66f818fc1175f959d
konstantin.borisov-at-908478757030 <borisovke@gmail.com>
2020-07-10 11:18:51 +0600

    Form validation added

--- DynamoDBSQL/dynamosql/src/app/gsi-solution/gsi-solution.component.ts
+++ DynamoDBSQL/dynamosql/src/app/gsi-solution/gsi-solution.component.ts
@@ -12,6 +12,7 @@ export class GsiSolutionComponent implements OnInit {
 
   primaryKey: string;
   sortKey: string = "";
+  resultKey: string = "";
   primaryKeyValue:string = "";
   sortKeyValue:string = "";
   descending: boolean = false;
