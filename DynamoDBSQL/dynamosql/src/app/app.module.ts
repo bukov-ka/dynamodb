576a91051e0ef09b2a28c93e533af656cfe924c1
konstantin.borisov-at-908478757030 <borisovke@gmail.com>
2020-07-24 02:12:01 +0600

    Side navbar, images cropped, styling

--- DynamoDBSQL/dynamosql/src/app/app.module.ts
+++ DynamoDBSQL/dynamosql/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';  
+import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -11,6 +11,9 @@ import { MatTableModule } from '@angular/material/table';
 import { MatExpansionModule } from '@angular/material/expansion'
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +25,6 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { QueryRunComponent } from './query-run/query-run.component';
 import { ConfirmationDialogComponent } from './confirmation-dialog/confirmation-dialog.component';
 import 'alasql';
-import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 
@@ -41,7 +43,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    BrowserAnimationsModule, 
+    BrowserAnimationsModule,
     MatInputModule,
     MatFormFieldModule,
     MatCheckboxModule,
@@ -51,7 +53,9 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatExpansionModule,
     MatIconModule,
     MatDialogModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
